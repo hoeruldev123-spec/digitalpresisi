c0ad130e16fd8ebad316e99e5260cb7d88d0fe8d
hoerul.dev123 <hoerul.dev123@gmail.com>
2026-05-19 19:47:48 +0700

    update api

--- wp-api-proxy.php
+++ wp-api-proxy.php
@@ -1,17 +1,26 @@
 <?php
-// wp-api-proxy.php
+
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
-$api_url = 'https://digitalpresisi.com/blog/index.php?rest_route=/wp/v2/posts?per_page=3&_embed';
+$api_url = 'https://digitalpresisi.com/blog/index.php?rest_route=/wp/v2/posts&per_page=3&_embed';
 
 $ch = curl_init();
+
 curl_setopt($ch, CURLOPT_URL, $api_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 $response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo json_encode([
+        'error' => curl_error($ch)
+    ]);
+    exit;
+}
+
 curl_close($ch);
 
 echo $response;
